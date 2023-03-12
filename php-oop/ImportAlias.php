8c2b2994791c419e18c5b387144de638cd980582
Ipnu Masyaid <masipnu1@gmail.com>
2023-03-13 00:29:39 +0700

    Penggunaan ALIAS
    ### ImportAlias.php
    Menggunakan alias untuk menandai semua yang di-import dari namespace agar mudah dalam penyebutannya.

---
+++ php-oop/ImportAlias.php
@@ -0,0 +1,15 @@
+<?php
+require_once "data/Conflict.php";
+require_once "data/Helper.php";
+
+use Data\One\Conflict as Conflict1;
+use Data\Two\Conflict as Conflict2;
+use function Helper\helpMe as help;
+use const Helper\APPLICATION as APP;
+
+$Conflict1 = new Conflict1();
+$Conflict2 = new Conflict2();
+
+help();
+
+echo APP . PHP_EOL;
