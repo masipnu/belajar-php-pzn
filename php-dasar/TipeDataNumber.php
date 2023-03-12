31dd76ab148d0ab5b0688bbc9597784047f56460
Ipnu Masyaid <masipnu1@gmail.com>
2023-03-13 01:10:45 +0700

    PHP Dasar
    ### HelloWorld.php
    - Membuat program hello world
    
    ### TipeDataNumber.php
    - Menggunakan Tipe data Integer dan Floating Point

---
+++ php-dasar/TipeDataNumber.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "Decimal : ";
+var_dump(12345);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111);
+
+echo "Underscore in number : ";
+var_dump(8_365_843_756);
+
+echo "Floating point : ";
+var_dump(1.234);
+
+echo "Floating point dengan E Notation (1.7 x 1.000) : ";
+var_dump(1.7e3); //pangkat 3
+
+echo "Floating point dengan E Notation minus (1.7 x 0.001) : ";
+var_dump(1.7e3);
+
+echo "Underscore di floating point : ";
+var_dump(1_234.123);
+
+echo "Integer overflow for 64 bit : ";
+var_dump(9223372036854775808); // kapasitas maksimum : 9223372036854775807
+
