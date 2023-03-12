5925eab7b227b0dd05f3f9e8186b54d2d1d9511b
Ipnu Masyaid <masipnu1@gmail.com>
2023-03-12 17:49:45 +0700

    Penerapan Function dan Constant dalam namespace
    ### Helper.php
    - Membuat namespace helper
    - Menambahkan fungsi helpMe()
    - Menamahkan konstanta APPLICATION
    
    ### Namespace.php
    - Memanggil namespace helper
    - Menjalankan function helpMe() yang dimiliki oleh namespace helper
    - Memanggil konstanta APPLICATION yang dimiliki oleh namespace helper
    ### Struktur OOP PHP.drawio
    - Skema struktur OOP dalam bahasa PHP

--- php-oop/Namespace.php
+++ php-oop/Namespace.php
@@ -1,8 +1,19 @@
 <?php
+
+// Memanggil namespace Data\One dan Data\Two
 require_once "data/Conflict.php";
+
+// Memanggil namespace helper
+require_once "data/Helper.php";
 
 $Conflict1 = new \Data\One\Conflict();
 $Conflict2 = new \Data\Two\Conflict();
 
 //var_dump($Conflict1);
 //var_dump($Conflict2);
+
+// Menampilkan konstanta yang berada di dalam namespace helper
+echo Helper\APPLICATION . PHP_EOL;
+
+// Memanggil fungsi yang berada di dalam namespace helper
+Helper\helpMe();
---
+++ php-oop/data/Helper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Helper;
+
+function helpMe(){
+    echo "HELP ME" . PHP_EOL;
+}
+
+const APPLICATION = "Belajar PHP OOP";
