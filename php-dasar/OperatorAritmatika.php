e0b247accceba8de2ae528c4daadd03330a001d1
Ipnu Masyaid <masipnu1@gmail.com>
2023-03-16 22:19:15 -1200

    Update Materi - Operator Aritmatika
    ### OperatorAritmatika.php
    - Pertambahan
    - Pengurangan
    - Perkalian
    - Pembagian
    - Modulo (sisa bagi)
    - Pangkat
    - Negatif dan Positif

---
+++ php-dasar/OperatorAritmatika.php
@@ -0,0 +1,54 @@
+<?php
+// Menggunakan operator aritmatika
+
+$x = 6;
+$y = 4;
+$z = 5;
+
+// Pertambahan
+$tambah = $x + $y;
+echo "Hasil dari $x + $y = $tambah \n";
+$tambah_negatif = -$tambah;
+echo "Jika dijadikan negatif menjadi $tambah_negatif \n";
+$tambah_positif = +$tambah;
+echo "Jika dijadikan positif menjadi $tambah_positif \n\n";
+
+// Pengurangan
+$kurang = $x - $y;
+echo "Hasil dari $x - $y = $kurang \n";
+$kurang_negatif = -$kurang;
+echo "Jika dijadikan negatif menjadi $kurang_negatif \n";
+$kurang_positif = +$kurang;
+echo "Jika dijadikan positif menjadi $kurang_positif \n\n";
+
+// Perkalian
+$kali = $x * $y;
+echo "Hasil dari $x x $y = $kali \n";
+$kali_negatif = -$kali;
+echo "Jika dijadikan negatif menjadi $kali_negatif \n";
+$kali_positif = +$kali;
+echo "Jika dijadikan positif menjadi $kali_positif \n\n";
+
+// Pembagian
+$bagi = $x / $y;
+echo "Hasil dari $x : $y = $bagi \n";
+$bagi_negatif = -$bagi;
+echo "Jika dijadikan negatif menjadi $bagi_negatif \n";
+$bagi_positif = +$bagi;
+echo "Jika dijadikan positif menjadi $bagi_positif \n\n";
+
+// Modulo
+$modulo = $x % $z;
+echo "Hasil dari $x % $z = $modulo \n";
+$modulo_negatif = -$modulo;
+echo "Jika dijadikan negatif menjadi $modulo_negatif \n";
+$modulo_positif = +$modulo;
+echo "Jika dijadikan positif menjadi $modulo_positif \n\n";
+
+// Pangkat
+$pangkat = $x ** $z;
+echo "Hasil dari $x pangkat $z = $pangkat \n";
+$pangkat_negatif = -$pangkat;
+echo "Jika dijadikan negatif menjadi $pangkat_negatif \n";
+$pangkat_positif = +$pangkat;
+echo "Jika dijadikan positif menjadi $pangkat_positif \n\n";
