17c7dc76f6a3129d42fc115728457406fa4ecbd9
masipnu <masipnu1@gmail.com>
2023-03-22 22:59:03 -0800

    Update Materi - Function Argument
    ### FunctionArgument.php
    - Tipe data jugling
    - Variable length argument list

--- php-dasar/FunctionArgument.php
+++ php-dasar/FunctionArgument.php
@@ -34,3 +34,42 @@ function sum(int $first, int $last){
 }
 
 $i=sum(3,2);
+
+// Konversi tipe data / type jugling
+// Jika tipe data memungkinkan untuk diconvert,
+// maka php akan menconvert (dalam kasus ini menjadi integer) dan memprosesnya
+sum(true,false);
+sum(20,"15");
+
+// Tipe data array tidak bisa diconvert
+//sum([],[]);
+
+// Variable length argument list
+// Cara mudah menentukan argument function berupa array
+
+// Contoh belum menggunakan Variable length argument list
+function sumAll(array $values){
+    $total = 0;
+    foreach ($values as $value) {
+        $total += $value;
+    }
+    echo "Total " . implode(" + ",$values) . " = $total" . PHP_EOL;
+}
+// Pemanggilan fungsi harus menambahkan argumen berupa array
+sumAll([5,6,7,4,5]);
+
+// Contoh sudah menggunakan Variable length argument list
+function sumTotal(...$values){
+    $total = 0;
+    foreach ($values as $value) {
+        $total += $value;
+    }
+    echo "Total " . implode(" + ",$values) . " = $total" . PHP_EOL;
+}
+// Ketika memanggil fungsi tidak perlu menambahkan argumen berupa array
+sumTotal(1,2,3,4,5);
+
+// Jika terlanjur membuat array sebagai argument,
+// dalam pemanggilan fungsi cukup ditambah ... di depan argumen
+$bilangan = [5,6,7,8,9];
+sumTotal(...$bilangan);
