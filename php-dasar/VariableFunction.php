aeb621321c1ab83d885933b9ba74d095bb76fe58
masipnu <masipnu1@gmail.com>
2023-03-24 13:14:40 +0700

    Update Materi - Variable Function
    ### VariableFunction.php
    - Penerapan Variable Function
    - Memanggil Built-in Function ke dalam argument function
    - Memanggil Custom Function ke dalam argument function

--- php-dasar/VariableFunction.php
+++ php-dasar/VariableFunction.php
@@ -1,6 +1,7 @@
 <?php
 // Menggunakan fitur variable function
 // Mengakses function dari value sebuah variable
+// Secara sederhana dapat diartikan = mengirim function ke dalam argumen function lain
 
 function foo(){
     echo "Ini fungsi foo" . PHP_EOL;
@@ -15,3 +16,27 @@ $panggilFoo();
 
 $panggilBar = "bar";
 $panggilBar();
+
+// Penerapan variable function - callback
+
+// Contoh-1
+function sayHello(string $name,$filter){
+    $finalName = $filter($name);
+    echo "Hello $finalName" . PHP_EOL;
+}
+// Memanggil fungsi sayHello()
+// dengan menambah parameter $filter dengan fungsi bawaan php
+sayHello("Eko","strtoupper");
+sayHello("Eko","strtolower");
+
+// Contoh-2
+function sayHi(string $name, $filter){
+    $finalName = $filter($name);
+    echo "Hello $finalName" . PHP_EOL;
+}
+// Meembuat contoh function untuk diparsing ke function sayHi()
+function sampleFunction(string $name): string {
+    return "Sample $name";
+}
+// MEmanggil fungsi sayHi() demgan paramater fungsi custom sampleFunction()
+sayHi("Eko","sampleFunction");
