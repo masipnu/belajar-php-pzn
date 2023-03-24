3f81ddf4e23c277c4ffc69993d5063ab8d63bd43
masipnu <masipnu1@gmail.com>
2023-03-24 15:43:40 +0700

    Update Materi - Komentar
    ### Komentar.php
    - Menggunakan Komentar
    - Komentar 1 baris
    - Komentar lebih dari 1 baris

---
+++ php-dasar/Komentar.php
@@ -0,0 +1,14 @@
+<?php
+// Menggunakan Komentar
+
+/*
+ * Ini komentar
+ * lebih dari
+ * satu baris
+ */
+
+function sayHello(){
+    // Komentari satu baris
+    echo "Hello" . PHP_EOL;
+}
+ sayHello();
