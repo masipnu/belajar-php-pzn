1212cf3f87fad0920cf39927c504e835fd834fe6
masipnu <masipnu1@gmail.com>
2023-03-26 23:40:54 +0700

    Sample Project - Aplikasi To-do List
    - Menambah View -> ViewAddTodoList.php

--- aplikasi-todolist/View/ViewAddTodoList.php
+++ aplikasi-todolist/View/ViewAddTodoList.php
@@ -1,6 +1,18 @@
 <?php
 
+require_once "../Model/TodoList.php";
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
 function viewAddTodoList()
 {
+    echo "MENAMBAH TO-DO" . PHP_EOL;
 
+    $todo = input("To-do : ");
+
+    if ($todo == "x"){
+        // batal
+    } else {
+        addTodoList($todo);
+    }
 }
