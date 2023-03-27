157e9b638ded9f0473d1d84ecfdf3958bd82366d
masipnu <masipnu1@gmail.com>
2023-03-27 15:16:47 +0700

    PHP OOP - Function Overriding
    - FunctionOverriding.php
    - Manager.php
    
        /*
         * Ini adalah Function Overriding
         * Function ini akan menimpa function dari class parent nya yaitu sayHello()
         * maka function sayHello() dari class Manager akan diabaikan
         */

--- php-oop/data/Manager.php
+++ php-oop/data/Manager.php
@@ -6,11 +6,19 @@ class Manager
 
     function sayHello(string $name):void
     {
-        echo "Hi $name, My name is $this->name" . PHP_EOL;
+        echo "Hi $name, My name is $this->name as Manager" . PHP_EOL;
     }
 }
 
 class VicePresident extends Manager
 {
-
+    /*
+     * Ini adalah Function Overriding
+     * Function ini akan menimpa function dari class parent nya yaitu sayHello()
+     * maka function sayHello() dari class Manager akan diabaikan
+     */
+    function sayHello(string $name):void
+    {
+        echo "Hi $name, My name is $this->name as Vice President" . PHP_EOL;
+    }
 }
